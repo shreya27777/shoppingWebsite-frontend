09cf87f19c27c32594bd542159196d03d2d341dc
shreya sharma <shreyasharmacu@gmail.com>
2019-10-11 14:01:00 +0530

    cart ui completed with all functionality

--- src/app/user-cart/user-cart.component.ts
+++ src/app/user-cart/user-cart.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AppService} from '../app.service';
+import {HttpService} from '../http.service';
 
 @Component({
   selector: 'app-user-cart',
@@ -8,33 +9,56 @@ import {AppService} from '../app.service';
   styleUrls: ['./user-cart.component.scss']
 })
 export class UserCartComponent implements OnInit {
-
-  demo = [1];
-  name = 'Shein off-shoulder black top';
-  quantity = 1;
-  price = 900;
-  finalprice = this.price;
+  totalPrice;
+  // tslint:disable-next-line:ban-types
+  private cartItem: Object = [];
 
 
-  constructor(private service: AppService, private router: Router) { }
+  constructor(private service: AppService, private router: Router, private httpService: HttpService) {
+  }
 
   ngOnInit() {
-    if ( !this.service.checkLogin()) {
-      this.router.navigate(['/home']);
+    if (!this.service.checkLogin()) {
+      this.router.navigate(['/login']);
+    } else {
+      this.httpService.getItemsCart().subscribe((data) => {
+        this.cartItem = data;
+        this.httpService.total().subscribe((total) => {
+          this.totalPrice = total;
+        });
+      });
     }
+
   }
 
   goToHome() {
     location.href = 'home';
   }
-  increment() {
-    this.quantity = this.quantity + 1;
-    this.finalprice = this.price * this.quantity;
-  }
-  decrement() { if (this.quantity > 1) {
-      this.quantity = this .quantity - 1;
-    }
-                this .finalprice = this .price * this.quantity;
+
+  decrement(id) {
+    this.httpService.decreaseQuantity(id).subscribe((data) => {
+      this.cartItem = data;
+      this.httpService.total().subscribe((total) => {
+        this.totalPrice = total;
+      });
+    });
   }
 
+  increment(id) {
+    this.httpService.increaseQuantity(id).subscribe((data) => {
+      this.cartItem = data;
+      this.httpService.total().subscribe((total) => {
+        this.totalPrice = total;
+      });
+    });
+  }
+
+  removeFromCart(id) {
+    this.httpService.remove(id).subscribe((data) => {
+      this.cartItem = data;
+      this.httpService.total().subscribe((total) => {
+        this.totalPrice = total;
+      });
+    });
+  }
 }
